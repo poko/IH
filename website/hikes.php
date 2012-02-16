6abb38f1914e36e6753c996c93e47fe1f1551c66
Polina Koronkevich <pkoronke@ausrzrpkoronke.main.corp>
2012-02-16 17:52:26 -0600

    hikes page wip.

---
+++ website/hikes.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+	<head>
+		<title>indeterminate hikes + | ecoarttech</title>
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+		<meta name="keywords" content="digital and networked environmental art, eco art, green cultural studies, eco criticism, conceptual art, video art, digital video, new media and digital art technologies, New York, NYC, Maine, USA" content="ECOARTTECH draws on ideas in digital art, critical theory, ecology, eco-art, environmental literature, environmental philosophy, ecocriticism, and environmentalist activism.">
+		<link href="eco_art11.css" rel="stylesheet" type="text/css" media="all" />
+        <link rel="SHORTCUT ICON" href="http://www.ecoarttech.net/images/favicon.ico" />
+<link href="../eco_art11.css" rel="stylesheet" type="text/css" />
+        <script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<style type="text/css">
+<!--
+body {
+	background-color: #666666;
+}
+.style1 {color: #0066FF}
+.style8 {font-size: 9px; color: black;}
+-->
+</style></head>
+
+	<body>
+		<table width="980" border="0" cellspacing="2" cellpadding="2" align="center">
+	  <tr>
+				<td align="left" valign="top" bgcolor="#CCCCCC"><a href="http://www.ecoarttech.net"><img src="../images/feature_work_ban_img.gif" width="980" height="64" border="0" /></a><br />
+				  <table width="980" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <th width="95" align="left" valign="top" bgcolor="#FFFFFF" scope="col"><table width="95" border="0" cellspacing="4" cellpadding="2">
+                        <tr>
+                          <th align="left" valign="top" scope="col"><a href="http://www.ecoarttech.net"><img src="../images/wmv_player.jpg" alt="" width="95" height="58" border="0" /></a><br />
+                            <a href="index.html"><br />
+                            </a> <a href="../about.html">about</a><br />
+                            <a href="../works.html">works</a><br />
+                            <a href="../events.html">news/events</a><br />
+                            <br />
+                            <a href="http://www.vimeo.com/ecoarttech" target="_blank">videos</a><br />
+                            <a href="http://www.ecoarttech.net/blog">research blog</a><br />
+                            <br />
+                            <a href="../downloads/index.html">press images</a><br />
+                            <a href="../contact.html">contact</a><br />
+                            <br />
+                            <a href="http://ecoarttech.us4.list-manage1.com/subscribe?u=99a487b9dc5ad90e627613744&amp;id=914c29f4db" target="_blank"><img src="../images/mailing_list.gif" alt="ecoarttech mailing list" width="92" height="32" border="0" /></a> <br />
+                            <br />
+                            <img src="images/nav_line.gif" alt="" width="90" height="1" /></th>
+                        </tr>
+                        <tr>
+                          <th align="left" valign="top" scope="row"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                              <th align="left" valign="top" bgcolor="#CCCCCC" scope="col"><img src="../images/facebook.gif" alt="" width="16" height="16" border="0" /> <a href="http://www.facebook.com/ecoarttech" target="_blank"> ecoarttech facebook group</a><br />
+                              </th>
+                            </tr>
+                          </table>
+                          </th>
+                        </tr>
+                      </table>
+                        <p><span class="small"><br />
+                      <br />
+                      <br />
+                      <br />
+                      </span></p></th>
+                      <td width="885" align="left" valign="top" scope="col">
+                        <br />
+                        <table width="872" border="0" cellspacing="2" cellpadding="2">
+                        <tr>
+                          <td width="58%" align="left" valign="top"><span class="bigHighlite"><strong>indeterminate hikes+</strong></span><strong><br />
+                            <span class="bodystylehighlitePROMPT">ecoarttech's wilderness-actualizing app</span><br />
+                            <br />
+                         <iframe src="http://player.vimeo.com/video/35096318?title=0&amp;byline=0&amp;portrait=0" width="495" height="278" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                          </strong>
+
+                          <strong>IH+ is an Android app that transforms everyday landscapes into sites of bio-cultural diversity and wild happenings</strong>.</strong> Generally devices of rapid communication and consumerism, smartphones are re-appropriated by IH+ as tools of environmental imagination and meditative wonder, renewing awareness of intertwining biological, cultural, and media ecologies and slowing us down at the same time. The app works by importing the rhetoric of wilderness into virtually any place accessible by Google Maps and encouraging its users to treat these locales as spaces worthy of the attention accorded to sublime landscapes, such as canyons and gorges. <br />
+                          <br />
+                          <strong>IH+ can be performed in two ways: <br />
+(1) as a participatory public event  led by artist-guides along with a basecamp installation (e.g. at festivals/exhibitions)<br />
+(2) as a self-guided excursion taken by a hiker equipped with an IH+-enabled smartphone. </strong></p>
+                            <p><span class="style1"><strong>Revision IH+ is now in alpha testing and will debut in mid February. <br />
+Join our email list on the left nav to subscribe and we will let you know as soon as it is released.</strong></span><br />
+<br />
+<img src="images/android_download2.gif" alt="indeterminate hikes android market download" width="166" height="57" border="0" /></p>
+                            <br />
+                            <br /></td>
+                          <td width="42%" align="left" valign="top"></p>
+                            <table width="100%" border="0" cellspacing="4" cellpadding="2">
+                              <tr>
+                                <td align="left" valign="top"><span class="bigHighlite">recent hikes</span><br /></td>
+                              </tr>
+                            </table>
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td align="left" valign="top" >
+                                
+<?php
+include '../scripts/db_open.php';
+
+$query = "SELECT hike_id, name, description, date, username FROM hikes order by hike_id desc limit 4";
+$result=mysql_query($query);
+$hikes = array();
+while ($hike = mysql_fetch_object($result)) {
+    // get vistas & actions
+	$query = sprintf("SELECT id, hike_id, vistas.action_id, longitude, latitude, date, note, photo, verbiage, action_type from vistas INNER JOIN vista_actions WHERE vistas.action_id = vista_actions.action_id AND hike_id = '%s'",
+					mysql_real_escape_string($hike->hike_id));
+	$res=mysql_query($query);
+	$vista_json = array();
+	while ($row = mysql_fetch_object($res)) {
+		$vista_json[] = $row;
+	}
+	$hike->vistas = $vista_json;
+    array_push($hikes, $hike);
+}
+
+foreach ($hikes as $h){
+echo "<table width=\"100%\" border=\"0\" cellspacing=\"4\" cellpadding=\"2\">";
+	echo "<tr>";
+		echo "<td align=\"left\" valign=\"top\"><a href=\"http://www.ecoarttech.net/ih_plus/web/hike.php?id=".$h->hike_id."\">";
+			echo "<img src=\"images/hike_example1_img.jpg\" width=\"355\" height=\"123\" border=\"0\" /></a></td>";
+	echo "</tr>";
+	echo "<tr>";
+		echo "<td align=\"left\" valign=\"top\" class=\"bodystyle\">";
+			echo "<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"1\">";
+				echo "<tr>";
+					echo "<td width=\"27%\" align=\"left\" valign=\"top\"><a href=\"http://www.ecoarttech.net/ih_plus/web/hike.php?id=".$h->hike_id."\"><img src=\"images/hike_example1b_img.jpg\" alt=\"hike image\" width=\"120\" height=\"90\" border=\"0\" /></a></td>";
+					echo "<td width=\"73%\" align=\"left\" valign=\"top\" class=\"bodystyle\"><strong>West Chestnut Street Hike -<br />";
+					echo "Pioneered by ".$h->username."</strong>.<br />";
+					echo "A Hike around Cestnut street with many interesting things along the way... <br />";
+					echo "<a href=\"http://www.ecoarttech.net/ih_plus/web/hike.php?id=".$h->hike_id."\">More&gt;</a></td>";
+				echo "</tr>";
+			echo "</table>";
+		echo "</td>";
+	echo "</tr>";
+echo "</table>";
+echo "<img src=\"images/line_img.jpg\" width=\"355\" height=\"1\" alt=\"line\" /><br />";
+}
+
+include '../scripts/db_close.php';
+?>
+                           
+                                <table width="100%" border="0" cellspacing="4" cellpadding="2">
+                                  <tr>
+                                    <td align="left" valign="top"><a href="hikepages/index.html">
+                                    <img src="images/hike_example2_img.jpg" alt="" width="355" height="123" border="0" /></a>
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td align="left" valign="top" class="bodystyle">
+                                    <table width="100%" border="0" cellspacing="1" cellpadding="1">
+                                      <tr>
+                                        <td width="27%" align="left" valign="top">
+                                        	<a href="hikepages/index.html">
+                                        	<img src="images/hike_example2b_img.jpg" alt="hike image" width="120" height="90" border="0" /></a>
+                                        </td>
+                                        <td width="73%" align="left" valign="top" class="bodystyle">
+                                        	<strong>Main Street Hike - <br />
+                                        	Pioneered by C. Peppermint</strong><br />
+                                         	A Hike around Main Street with many interesting things along the way... <br />
+                                          <a href="hikepages/index.html">More&gt;</a>
+                                        </td>
+                                      </tr>
+                                    </table>
+                                      <a href="poko_hike.html"></a></td>
+                                  </tr>
+                                </table>
+                                <img src="images/line_img.jpg" width="355" height="1" alt="line" /><br />
+                                <table width="100%" border="0" cellspacing="4" cellpadding="2">
+                                  <tr>
+                                    <td align="left" valign="top"><a href="hikepages/index.html"><img src="images/hike_example3_img.jpg" alt="" width="355" height="123" border="0" /></a></td>
+                                  </tr>
+                                  <tr>
+                                    <td align="left" valign="top" class="bodystyle"><table width="100%" border="0" cellspacing="1" cellpadding="1">
+                                      <tr>
+                                        <td width="27%" align="left" valign="top"><a href="hikepages/index.html"><img src="images/hike_example3b_img.jpg" alt="hike image" width="120" height="90" border="0" /></a></td>
+                                        <td width="73%" align="left" valign="top" class="bodystyle"><strong>Beecher Playground Hike - <br />
+                                        Pioneered by L. Nadir</strong><br />
+                                          A Hike around  the playground with many interesting things along the way... <br />
+                                          <a href="hikepages/index.html">More&gt;</a></td>
+                                      </tr>
+                                    </table>
+                                      <a href="poko_hike.html"></a></td>
+                                  </tr>
+                                </table>
+                                <img src="images/line_img.jpg" width="355" height="1" alt="line" /><br />
+                                <table width="100%" border="0" cellspacing="4" cellpadding="2">
+                                  <tr>
+                                    <td align="left" valign="top"><a href="hikepages/index.html"><img src="images/hike_example4_img.jpg" alt="" width="355" height="123" border="0" /></a></td>
+                                  </tr>
+                                  <tr>
+                                    <td align="left" valign="top" class="bodystyle"><table width="100%" border="0" cellspacing="1" cellpadding="1">
+                                      <tr>
+                                        <td width="27%" align="left" valign="top"><a href="hikepages/index.html"><img src="images/hike_example4b_img.jpg" alt="hike image" width="120" height="90" border="0" /></a></td>
+                                        <td width="73%" align="left" valign="top" class="bodystyle"><strong>River Hike - <br />
+                                        Pioneered J.Colivita</strong><br />
+                                          A Hike around the rivert with many interesting things along the way... <br />
+                                          <a href="hikepages/index.html">More&gt;</a></td>
+                                      </tr>
+                                    </table>
+                                      <a href="poko_hike.html"></a></td>
+                                  </tr>
+                                </table>
+                                <img src="images/line_img.jpg" width="355" height="1" alt="line" /><br /></td>
+                              </tr>
+                          </table>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                          <br /></td>
+                          </tr>
+                      </table>
+                        <table width="747" border="0" cellspacing="2" cellpadding="2">
+                        <tr>
+                          <td width="66%"><p>&nbsp;</p></td>
+                          </tr>
+                      </table>
+                      <p><br />
+                     </p>
+                      <table width="100%" border="0" cellspacing="4" cellpadding="2">
+                        <tr>
+                          <td align="left" valign="top" scope="col"><span class="small"><img src="images/nav_line.gif" alt="" width="840" height="1" /><br />
+                            <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/"><br />
+                              <img alt="Creative Commons License" style="border-width:0" src="http://creativecommons.org/images/public/somerights20.png"/><br />
+                          </a><br/>
+                            This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 Unported License</a><br />
+                            <br />
+                            This work is made possible in part by  contributions from the New York Foundation for the Arts (NYFA), New York State Council on the Arts, The University of Rochester, <font face="Arial, Helvetica, sans-serif">the<a href="http://www.hartwick.edu/x446.xml" target="_blank"> Pine Lake Environmental Campus</a> of Hartwick College<font face="Arial, Helvetica, sans-serif">, and</font></font></span> <span class="small"><font face="Arial, Helvetica, sans-serif"><a href="http://www.turbulence.org/" target="_blank">Turbulenc</a></font></span><span class="style8"><font face="Arial, Helvetica, sans-serif"><a href="http://www.turbulence.org/" target="_blank">e.org</a></font></span><span class="small"><font face="Arial, Helvetica, sans-serif">. A</font></span><span class="style8"><font face="Arial, Helvetica, sans-serif">ll canines featured in these works are rescued <a href="http://akitarescuewny.com/ARWNY/facts.htm" target="_blank">Akitas</a> brought into our inter-species family via <a href="http://www.akitarescuewny.com/" target="_blank">Akita Rescue of Western New York</a>.</font></span></td>
+                        </tr>
+                      </table></td>
+                    </tr>
+                  </table>	    </td>
+	  </tr>
+		</table>
+
+</body>
+
+</html>
