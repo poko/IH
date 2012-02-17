3834b6213abcec15fdd89966b734e2b9bc0e25e7
Polina Koronkevich <pkoronke@ausrzrpkoronke.(none)>
2012-02-17 11:07:34 -0600

    UI tweaks - added AboutActivity

--- android/src/net/ecoarttech/ihplus/AboutActivity.java
+++ android/src/net/ecoarttech/ihplus/AboutActivity.java
@@ -4,26 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-public class IntroActivity extends Activity {
+public class AboutActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.intro);
-
-		Toast.makeText(this, "make sure gps is enabled.", Toast.LENGTH_LONG).show();
-	}
-	
-	public void onAboutClick(View v){
-		startActivity(new Intent(this, AboutActivity.class));
+		setContentView(R.layout.about);
 	}
 	
 	public void onCreditsClick(View v){
 		startActivity(new Intent(this, InfoActivity.class));
 	}
-
+	
 	public void onHikeClick(View v) {
 		Intent i = new Intent(this, CreateHikeActivity.class);
 		startActivity(i);
@@ -33,4 +26,5 @@ public class IntroActivity extends Activity {
 		Intent i = new Intent(this, SearchActivity.class);
 		startActivity(i);
 	}
+
 }
